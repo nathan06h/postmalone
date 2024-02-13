9a4ce853923caabcb3a4232368b2fd6bdc176565
thanhthanh <thanhthanh@usernoMacBook-Pro.local>
2024-02-13 20:15:35 +0900

    new project for 6months

---
+++ assets/setsong.js
@@ -0,0 +1,12 @@
+ 
+ /*
+ 
+ var audio = document.getElementById("myAudio");
+    function toggleAudio() {
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+  */ 
